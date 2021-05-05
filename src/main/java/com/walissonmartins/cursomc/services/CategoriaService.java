7d6366cc9d4cdfc044c64caf1570d1e5245ef0b5
Walisson Martins <walisson.martinsti@gmail.com>
2021-05-05 18:36:42 -0300

    Endpoint /cliente/{id} disponível

--- src/main/java/com/walissonmartins/cursomc/services/CategoriaService.java
+++ src/main/java/com/walissonmartins/cursomc/services/CategoriaService.java
@@ -15,10 +15,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) { 
-		
-		 Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElseThrow(() -> new ObjectNotFoundException( 
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
-		} 
+	public Categoria buscar(Integer id) {
+
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 }
---
+++ src/main/java/com/walissonmartins/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.walissonmartins.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.walissonmartins.cursomc.domain.Cliente;
+import com.walissonmartins.cursomc.repositories.ClienteRepository;
+import com.walissonmartins.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
