c391b31efeef05e2f45373656b4c42acee2b7350
Walisson Martins <walisson.martinsti@gmail.com>
2021-05-06 11:05:26 -0300

    ItemPedido e ItemPedidoPk

--- src/main/java/com/walissonmartins/cursomc/CursomcApplication.java
+++ src/main/java/com/walissonmartins/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.walissonmartins.cursomc.domain.Cidade;
 import com.walissonmartins.cursomc.domain.Cliente;
 import com.walissonmartins.cursomc.domain.Endereco;
 import com.walissonmartins.cursomc.domain.Estado;
+import com.walissonmartins.cursomc.domain.ItemPedido;
 import com.walissonmartins.cursomc.domain.Pagamento;
 import com.walissonmartins.cursomc.domain.PagamentoComCartao;
 import com.walissonmartins.cursomc.domain.PagamentoDeboleto;
@@ -25,6 +26,7 @@ import com.walissonmartins.cursomc.repositories.CidadeRepository;
 import com.walissonmartins.cursomc.repositories.ClienteRepository;
 import com.walissonmartins.cursomc.repositories.EnderecoRepository;
 import com.walissonmartins.cursomc.repositories.EstadoRepository;
+import com.walissonmartins.cursomc.repositories.ItemPedidoRepository;
 import com.walissonmartins.cursomc.repositories.PagamentoRepository;
 import com.walissonmartins.cursomc.repositories.PedidoRepository;
 import com.walissonmartins.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -114,7 +119,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Pagamento pagto2 = new PagamentoDeboleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
 				null);
-		
+
 		ped2.setPagamento(pagto2);
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
@@ -122,5 +127,19 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 0.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
+		
 	}
 }
--- src/main/java/com/walissonmartins/cursomc/domain/Produto.java
+++ src/main/java/com/walissonmartins/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.walissonmartins.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -23,13 +26,23 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
-	
+
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+
+		return lista;
+	}
+
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA", 
-		joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 
@@ -72,6 +85,14 @@ public class Produto implements Serializable {
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
