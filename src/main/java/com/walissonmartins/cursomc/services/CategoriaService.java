e86fd9321ab0b8afbbc7c0e29dec7506c2a91110
Walisson Martins <walisson.martinsti@gmail.com>
2021-05-06 19:11:59 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/walissonmartins/cursomc/services/CategoriaService.java
+++ src/main/java/com/walissonmartins/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.walissonmartins.cursomc.domain.Categoria;
@@ -43,8 +46,15 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir a categoria que possuí produtos");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+		
 	}
 }
