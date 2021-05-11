7673f58762167a5a9f54af8c5dcbd1765374c9db
Walisson Martins <walisson.martinsti@gmail.com>
2021-05-11 20:15:15 -0300

    total e subtotal do pedido

--- src/main/java/com/walissonmartins/cursomc/domain/Pedido.java
+++ src/main/java/com/walissonmartins/cursomc/domain/Pedido.java
@@ -54,6 +54,15 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Double getValotTotal() {
+		double soma = 0.0;
+		for(ItemPedido ip: itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
