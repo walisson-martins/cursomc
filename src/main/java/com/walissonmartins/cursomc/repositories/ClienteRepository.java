830c3ff5ee9935a5f93de55c233b865766a8daef
Walisson Martins <walisson.martinsti@gmail.com>
2021-05-11 18:57:53 -0300

    email não inserido na validação

--- src/main/java/com/walissonmartins/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/walissonmartins/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,15 @@
 package com.walissonmartins.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.walissonmartins.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente finByEmail(String email);
 }
