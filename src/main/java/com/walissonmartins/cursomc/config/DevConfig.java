7962fa68f9b8545c4abc054c0270ad2275bf805c
Walisson Martins <walisson.martinsti@gmail.com>
2021-05-12 19:27:37 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/walissonmartins/cursomc/config/DevConfig.java
+++ src/main/java/com/walissonmartins/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.walissonmartins.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,15 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.walissonmartins.cursomc.services.DBService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbservice;
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
 		dbservice.instantiateTestDatabase();
 		return true;
 	}
